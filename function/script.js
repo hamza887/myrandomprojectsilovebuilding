43154fcd38e1a67e0ea7854434b22043fe8e39c6
hamza887 <hamzabutt887>
2021-06-05 11:13:43 -0400

    added some more functions just the practise

--- function/script.js
+++ function/script.js
@@ -1,32 +1,56 @@
-//document.write('hwllo world');
+// //document.write('hwllo world');
 
 
-    const printsum=()=>{
-         console.log(10+10);
-    }
+//     const printsum=()=>{
+//          console.log(10+10);
+//     }
 
-    printsum();
+//     printsum();
 
-    const printss=()=>{
-        let count=1;
-        let x='#';
-        for (let i = 1; i <5; i++) {
-            console.log(x.repeat(count));
-            count+=1;
+//     const printss=()=>{
+//         let count=1;
+//         let x='#';
+//         for (let i = 1; i <5; i++) {
+//             console.log(x.repeat(count));
+//             count+=1;
             
-        }
-    }
-    printss();
+//         }
+//     }
+//     printss();
 
-    function generatePyramid() {
-     //   var totalNumberofRows = 5,
-            arr;
-        for (var i = 1; i <= 5; i++) {
-            arr = [];
-            for (var j = 1; j <= i; j++) {
-                arr.push(j);            
-            }
-            console.log(arr.join(" ") + "\n");
-        }
+//     // function generatePyramid() {
+//     //  //   var totalNumberofRows = 5,
+//     //       var  arr;
+//     //     for (var i = 1; i <= 5; i++) {
+//     //         arr = [];
+//     //         for (var j = 1; j <= i; j++) {
+//     //             arr.push(j);            
+//     //         }
+//     //         console.log(arr.join(" ") + "\n");
+//     //     }
+//     // }
+//     // generatePyramid();
+
+
+//     const generatstuff=()=>{
+//         let arrr='#';
+//         for (let i = 1; i <=5; i++) {
+//             for (let j = 0; j <=i; j++) {
+//                 arrr.push(j);
+                
+//             }
+//             console.log(arrr)
+            
+//         }
+//     }
+//    generatstuff();
+
+
+const checkinput=(input)=>{
+    if(input.length>10){
+        console.log("hello i think this is greater then 10");
+    }else{
+        console.log("you are certainly getting an error");
     }
-    generatePyramid();
+}
+   checkinput('helliidhsiddf')
