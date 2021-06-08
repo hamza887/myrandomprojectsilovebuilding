53cc0899ac0d5dde35721e22bcf78a0f8efd2392
hamza887 <hamzabutt887>
2021-06-08 07:10:14 -0400

    adding doms

--- function/script.js
+++ function/script.js
@@ -1,23 +1,31 @@
 
-
-  class rectangle{
-      constructor(height, width,newvalue){
-          this.height=height;
-          this.width=width;
-          this.newvalue=newvalue;
-      }
-      get area(){
-          return this.calcArea();
-      }
-      calcArea(){
-          return this.height*this.width; 
-      }
-      output(){
-          console.log(`the new rectangle value is `+this.calcArea())
-      }
+  const hi=(hi)=>{
+      return hi
   }
-  const rec= new rectangle(10,10,10);
 
-   rec.output();
+
+   const hello= document.getElementById('hello');
+
+      hello.innerHTML='hellow how are you';
+      hello.style.backgroundColor='red';
+      hello.style.color='green';
+
+      const para= document.getElementById('paragrapgh')
+      para.innerHTML= `${hi('hello  this is new paragrapgh')}`;
+      para.style.color='red';
+
+      const titleid= para.setAttribute('id','hellooo');
+      console.log(titleid)
+
+       const aaa=document.getElementById('aa')
+         aaa.setAttribute('href','google.com');
+         aaa.setAttribute('target','_blank')
+
+        let div=document.getElementById('great');
+        console.log(div.dataset)
+      
+     const mylistitems=document.getElementById('li');
+
+       
 
    
